57700339943e099c449bdbd712890b1d00cdf41d
Daniel Berlin <dberlin@dberlin.org>
2015-02-04 11:14:30 -0800

    First working version of heap version
    Refactor a little bit
    
    Handle def && use properly
    
    Chunking away
    
    First somewhat working version of New GVN + Memory SSA
    
    Fix debug output
    
    Move memory SSA into a class
    
    Comments and a bug fix
    
    comments
    
    Get phi nodes working (not well tested)
    
    Comments
    
    Move memory SSA over to it's own file
    
    Next version of memorySSA

--- include/llvm/InitializePasses.h
+++ include/llvm/InitializePasses.h
@@ -206,6 +206,7 @@ void initializeMergedLoadStoreMotionPass(PassRegistry &);
 void initializeMetaRenamerPass(PassRegistry&);
 void initializeMergeFunctionsPass(PassRegistry&);
 void initializeModuleDebugInfoPrinterPass(PassRegistry&);
+void initializeNewGVNPass(PassRegistry&);
 void initializeNoAAPass(PassRegistry&);
 void initializeObjCARCAliasAnalysisPass(PassRegistry&);
 void initializeObjCARCAPElimPass(PassRegistry&);
